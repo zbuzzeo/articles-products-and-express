a3d2c3efe5dc5bce166c31c458a5c27f1d2ecd32
Raymond Luong <raymondluong@Raymonds-MBP.lan>
2018-11-02 15:24:37 -1000

    sanity check

---
+++ app.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const app = express();
+
+
+// app.get('/', () => {
+//   res.send('hello AFDADFF')
+// })
+
+// app.listen(8989, () => {
+//   console.log('listening on port 8989')
+// })
+
+console.log('hJ*****iaADFOHAPOIDHAOAIDFHPOAIDFHOPIADsdf')
