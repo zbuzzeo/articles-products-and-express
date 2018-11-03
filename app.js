ec4ab8268cdc8f46183c3cdf9aa3067222fc9d67
Raymond Luong <raymondluong@Raymonds-MBP.lan>
2018-11-02 16:32:35 -1000

    pristine condition

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-
-=======
-// const express = require('express');
-// const app = express();
-
-
-// app.get('/', () => {
-//   res.send('hello AFDADFF')
-// })
-
-// app.listen(8989, () => {
-//   console.log('listening on port 8989')
-// })
-
-console.log('hJ*****iaADFOHAPOIDHAOAIDFHPOAIDFHOPIADsdf')
->>>>>>> a3d2c3efe5dc5bce166c31c458a5c27f1d2ecd32
+console.log('sanity check');
